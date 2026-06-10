92b8472bb5f0ebf59bd8f70300f78517d75115e3
vsvkhs <61653054+vsvkhs@users.noreply.github.com>
2026-06-10 15:29:27 +0800

    Update diy-part1.sh

--- HY3000-hanwckf/diy-part1.sh
+++ HY3000-hanwckf/diy-part1.sh
@@ -17,4 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
